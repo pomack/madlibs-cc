e420a6fdcb79b89e91cb086fd0f89789fdf9a07b
lenin <lenin@mbp.local>
2011-05-14 12:49:20 -0400

    created article

--- js/collabralibs.js
+++ js/collabralibs.js
@@ -1,5 +1,16 @@
 // Self loading-function
 (function () {
     var element = $('#mainContainer');
-    console.log(element);
+    //console.log(element);
 })();
+
+$(function () {
+
+	$('#mainContainer').bind('mouseup', function () { 
+		var r = window.getSelection().getRangeAt(0).cloneRange();
+		if (r.toString() == '') console.log('nothing selected'); else console.log(r.toString());
+	}
+);
+	
+});
+
